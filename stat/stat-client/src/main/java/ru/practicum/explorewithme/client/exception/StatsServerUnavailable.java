e90d253b73be2b26a85e590d6ed566270dbe32e9
twiksokyan <twkscode@gmail.com>
2026-08-14 01:05:35 +0300

    add congig and gateway servers

--- stat/stat-client/src/main/java/ru/practicum/explorewithme/client/exception/StatsServerUnavailable.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.explorewithme.client.exception;
-
-public class StatsServerUnavailable extends RuntimeException {
-    public StatsServerUnavailable(String message) {
-        super(message);
-    }
-}
