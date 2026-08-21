03c2b0d678546f75b9b316cb29da4ccb12adcbf1
twiksokyan <twkscode@gmail.com>
2026-08-21 03:33:39 +0300

    fix similarity calculation in aggragator

--- stats/aggregator/src/main/java/ru/yandex/practicum/ewm/aggregator/service/EventSimilarityCalculatorImpl.java
+++ stats/aggregator/src/main/java/ru/yandex/practicum/ewm/aggregator/service/EventSimilarityCalculatorImpl.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.ewm.aggregator.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.practicum.ewm.stats.avro.ActionTypeAvro;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class EventSimilarityCalculatorImpl implements EventSimilarityCalculator {
     @Value("${user.action.weight.like}")
     private Double LIKE_WEIGHT;
@@ -33,20 +35,35 @@ public class EventSimilarityCalculatorImpl implements EventSimilarityCalculator 
 
         List<EventSimilarityAvro> eventSimilarities = new ArrayList<>();
 
-        Map<Long, Double> eventWeights = eventUserWeight.getOrDefault(eventId, new HashMap<>());
+        log.debug("Отображение с весами пользователей для мероприятий содержит ключ - {}: {}",
+                eventId, eventUserWeight.containsKey(eventId));
+        eventUserWeight.putIfAbsent(eventId, new HashMap<>());
+        Map<Long, Double> eventWeights = eventUserWeight.get(eventId);
 
         Double newWeight = mapRating(event.getActionType());
+
+        if (!eventWeights.containsKey(userId)) {
+            log.debug("Первое взаимодействие пользователя - {} с мероприямием - {}. Добавляем его оценку сумме оценок.",
+                    userId, eventId);
+            eventTotalWeight.merge(eventId, newWeight, Double::sum);
+        }
+
         eventWeights.putIfAbsent(userId, newWeight);
 
         Double currentWeight = eventWeights.get(userId);
         if (!needUpdateWeight(currentWeight, newWeight)) {
-            // TODO nothing to return
+            log.debug("Для события - {} у пользователя {} новая оценка {} не отличается от старой - {}", eventId, userId, newWeight, currentWeight);
+            return new ArrayList<>();
         }
+        eventWeights.put(userId, newWeight);
         // обновляем суммы
         Double weightDelta = newWeight - currentWeight;
+        log.debug("Разница между оценками для события {} от пользователя {} - {}", eventId, userId, weightDelta);
         // currentWeight по дефолту, так как если сумма пустая, то первое значение - вес текущего мероприятия
         eventTotalWeight.merge(eventId, weightDelta, Double::sum);
+        log.debug("Обновили сумму оценок для события {}", eventId);
 
+        log.debug("Начало пересчета коэффициентов");
         for (Map.Entry<Long, Map<Long, Double>> entry : eventUserWeight.entrySet()) {
             Long eventForCompareId = entry.getKey();
 
@@ -55,12 +72,18 @@ public class EventSimilarityCalculatorImpl implements EventSimilarityCalculator 
             }
 
             Map<Long, Double> comparingEventUserWeights = entry.getValue();
+            log.debug("Пользователь - {} взаимодействовал с событием для сравнения - {}: {}",
+                    userId, eventForCompareId, comparingEventUserWeights.containsKey(userId));
             if (comparingEventUserWeights.containsKey(userId)) {
+                log.debug("Пересчет коэффициента между событиями {} и {}", eventId, eventForCompareId);
                 Double comparingEventUserWeight = comparingEventUserWeights.get(userId);
+                log.debug("Оценка пользователя событию {} для сравнения - {}", eventForCompareId, comparingEventUserWeight);
 
                 Double oldMinContribution = Math.min(currentWeight, comparingEventUserWeight);
                 Double newMinContribution = Math.min(newWeight, comparingEventUserWeight);
                 Double deltaContribution = newMinContribution - oldMinContribution;
+
+                log.debug("oldMinContribution = {}, newMinContribution - {}", oldMinContribution, newMinContribution);
 
                 Long eventA = Math.min(eventId, eventForCompareId);
                 Long eventB = Math.max(eventId, eventForCompareId);
@@ -71,11 +94,14 @@ public class EventSimilarityCalculatorImpl implements EventSimilarityCalculator 
                 Double sMin = eventPairsMinSum.get(eventA).get(eventB);
                 Double sumEventA = eventTotalWeight.get(eventA);
                 Double sumEventB = eventTotalWeight.get(eventB);
+                log.debug("Числитель - {}, знаменатель для А - {}, знаменатель для B - {}", sMin, sumEventA, sumEventB);
 
                 double score = 0.0;
                 if (sumEventA > 0 && sumEventB > 0) {
+                    log.debug("Переситываес коэффициент");
                     score = sMin / (Math.sqrt(sumEventA) * Math.sqrt(sumEventB));
                 }
+                log.debug("Новый коэффициент - {}", score);
 
                 EventSimilarityAvro similarity = EventSimilarityAvro.newBuilder()
                         .setEventA(eventA)
@@ -83,10 +109,12 @@ public class EventSimilarityCalculatorImpl implements EventSimilarityCalculator 
                         .setScore(score)
                         .setTimestamp(event.getTimestamp())
                         .build();
-
+                log.debug("Добавляем новый объект для ответа с параметрами: eventA - {}, eventB = {}, score - {}",
+                        eventA, eventB, score);
                 eventSimilarities.add(similarity);
             }
         }
+        log.debug("Конец пересчета коэффициентов. Пересчитано - {}", eventSimilarities.size());
         return eventSimilarities;
     }
 
